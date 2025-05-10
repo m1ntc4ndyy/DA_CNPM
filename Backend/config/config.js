75c1ad12d10f05a7a3d234b11a0fc02c927ea0c6
m1ntc4ndyy <123210141@sv1.dut.udn.vn>
2025-05-10 09:29:12 +0700

    add env

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || 'abcd1234',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'school_event_management',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
